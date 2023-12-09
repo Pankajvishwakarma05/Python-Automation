6755c38e03507616eb86f2c023240b2d41144835
SWATI <swatishinde9271@gmail.com>
2023-12-09 14:57:25 +0530

    swati updated code

--- locator.py
+++ locator.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service_obj = Service ("C:/Users/vishw/Desktop/Personal/chromedriver-win64/chromedriver.exe")
+service_obj = Service ("C:/Users/SWATI/Desktop/webdriver/chromedriver.exe")
 driver = webdriver.Chrome(service= service_obj)
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
@@ -13,11 +13,13 @@ driver.get("https://rahulshettyacademy.com/angularpractice/")
 # XPATH  //tagname[@attribute='value']
 # CSS  tagname[attribute='value']
 
-driver.find_element(By.XPATH, "//input[@name='name']").send_keys("Pankaj vishwakarma")
-driver.find_element(By.CSS_SELECTOR, "input[name='email']").send_keys("deo@gmail.com")
-driver.find_element(By.ID, "exampleInputPassword1").send_keys("Pv123456")
+driver.find_element(By.XPATH, "//input[@name='name']").send_keys("Swati Shinde")
+driver.find_element(By.CSS_SELECTOR, "input[name='email']").send_keys("swati@gmail.com")
+driver.find_element(By.ID, "exampleInputPassword1").send_keys("ss123")
 driver.find_element(By.ID, "exampleCheck1").click()
-driver.find_element(By.CSS_SELECTOR, "input[value='Submit']").click()
+driver.find_element(By.ID, "exampleFormControlSelect1").send_keys("Female")
+driver.find_element(By.ID, "inlineRadio1").click()
+driver.find_element(By.CSS_SELECTOR, "input[name='bday']").send_keys("28/09/1998")
 driver.find_element(By.CSS_SELECTOR, "input[value='Submit']").click()
 
 time.sleep(60)
